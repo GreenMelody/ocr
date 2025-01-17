1dfae9f00431f35865513ce4b641470589235ccb
minsu kim <cafri2580@gmail.com>
2025-01-17 15:06:32 +0900

    add preprocess img
    Signed-off-by: minsu kim <cafri2580@gmail.com>

--- app2.py
+++ app2.py
@@ -31,28 +31,49 @@ def process_image():
         ori_img = image
 
         # 이미지를 흑백으로 변환
-        image = image.convert("L")
+        image_np = np.array(image)
+        # image = image.convert("L")
+        # img_gray = cv2.cvtColor(image_np, cv2.COLOR_RGB2GRAY)  # 흑백 변환
+        # img_binary = cv2.threshold(img_gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]  # OTSU 이진화
+
+        # 이미지 전처리
+        image_np = np.array(image)
+        img_gray = cv2.cvtColor(image_np, cv2.COLOR_RGB2GRAY)
+        img_binary = cv2.threshold(img_gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+        # 이미지 확대
+        scale = 2
+        height, width = img_binary.shape[:2]
+        img_binary = cv2.resize(img_binary, (width * scale, height * scale), interpolation=cv2.INTER_CUBIC)
+
+        # 잡음 제거
+        img_binary = cv2.medianBlur(img_binary, 3)
+
+        # 경계선 강조
+        kernel = np.ones((1, 1), np.uint8)
+        img_binary = cv2.dilate(img_binary, kernel, iterations=1)
+        img_binary = cv2.erode(img_binary, kernel, iterations=1)
 
         # Tesseract OCR 실행
-        # tesseract_config = f'--psm 6 -l {lang_code}'
-        tesseract_config = f'--psm 3 -l {lang_code}'
-        recognized_text = pytesseract.image_to_string(image, config=tesseract_config)
+        tesseract_config = f'--psm 6 -l {lang_code}'
+        # tesseract_config = f'--psm 3 -l {lang_code}'
+        recognized_text = pytesseract.image_to_string(image_np, config=tesseract_config)
 
         # Tesseract에서 바운딩 박스 가져오기
         boxes = pytesseract.image_to_boxes(image, config=tesseract_config)
 
         # 바운딩 박스를 OpenCV를 사용해 이미지에 그리기
-        image_np = np.array(ori_img)  # PIL 이미지를 NumPy 배열로 변환
-        h, w, _ = image_np.shape
+        ori_image_np = np.array(ori_img)  # PIL 이미지를 NumPy 배열로 변환
+        h, w, _ = ori_image_np.shape
         for box in boxes.splitlines():
             b = box.split()
             x1, y1, x2, y2 = map(int, (b[1], b[2], b[3], b[4]))
             y1, y2 = h - y1, h - y2  # Tesseract 좌표계 변환
-            cv2.rectangle(image_np, (x1, y2), (x2, y1), (0, 255, 0), 1)
+            cv2.rectangle(ori_image_np, (x1, y2), (x2, y1), (0, 255, 0), 1)
 
-        image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
+        ori_image_np = cv2.cvtColor(ori_image_np, cv2.COLOR_BGR2RGB)
         # 바운딩 박스가 그려진 이미지를 Base64로 변환
-        _, buffer = cv2.imencode('.png', image_np)
+        _, buffer = cv2.imencode('.png', ori_image_np)
         image_with_boxes = base64.b64encode(buffer).decode('utf-8')
 
         return jsonify({
